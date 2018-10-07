acc7c45812e52f57ad77a7d1304d3695aca438bb
Locke Monstah <Mloughran87@gmail.com>
2018-10-06 22:09:40 -0400

    save5

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,57 @@
-//Setting variables and zeroing counters (`var attempts` set to 9 yeilds 10 total attempts as numbering begins with zero)
-var won = 0;
-var lost = 0;
-var attempts = 10;
-var usedArray = [];
-var ranLetter = ranLetter;
-var letters = "qwertyuiopasdfghjklzxcvbnm"
+   //setting up the variables, wins, losses, guesses, guesses made by user
+    // and the random letter the computer chooses, finally an array of the total letters
+    var win = 0;
+    var loss = 0;
+    var guesses = 10;
+    var guessesSofar = [];
+    var randomLetter = randomletter;
+    var letters = "qwertyuiopasdfghjklzxcvbnm"
 
-//GENERATING THE COMPUTERS SECRET LETTER
-//Generates random number (decimal from 0 to 1) which is multiplied by 26 and the rounded down to the nearest whole number by the Math.floor method. 
-ranLetter = letters[Math.floor(Math.random() * letters.length)];
-console.log(ranLetter);
+        // random generator, from Crystal example 
+    randomLetter = letters[Math.floor(Math.random() * letters.length)];
+    console.log(randomLetter);
 
-//Uses random whole number generated above to select a random letter from the array [letters] and assigns it to the var ranLetter
-function jsGuess() {
-      ranLetter = letters[Math.floor(Math.random() * letters.length)];
-      console.log(ranLetter);
+    function jsGuess(){
+        randomLetter = letters[Math.floor(Math.random() * letters.length)]
+        console.log(randomLetter)
+    }
+        // using onkeyup to capture player input 
 
-}
+    document.onkeyup = function (event){
+        var playerGuess = event.key; 
 
-//CAPTURING THE PLAYERS INPUT
-//.onkeyup captures the players input as playerGuess
-document.onkeyup = function (event) {
-      var playerGuess = event.key;
+        if (playerGuess === randomLetter ){
+            win++;
+            guesses = 10
+            guessesSofar = [];
+        } 
 
-      // HANDELING CORRECT GUESSES
-      //test if players guess equals ranLetter, if true it increments wins by 1, and clears used letters array. Supposed to reset guess # to 10 but starts at 9 instead, 
-      if (playerGuess === ranLetter) {
-            won++;
-            attempts = 10;
-            usedArray = [];
+        jsGuess();
+        if (playerGuess !== randomLetter){
+            guesses--;
 
-      }
+        }
 
-      //HANDELING INCORRECT GUESSES
-      //tests if players guess Does Not Equal ranLetter and decriments attempts by 1
-      jsGuess();
-      if (playerGuess !== ranLetter) {
-            attempts--;
+        if (guesses == 0){
+            loss++;
+            guessesSofar = []
+            guesses = 10;
+        }
 
-      }
+        if (guessesSofar.indexOf(playerGuess) >=0){
 
-      //when remaining attempts equals zero, lost is incrimented by 1; attempts is reset to 10, and used letters array is cleared
-      if (attempts == 0) {
-            lost++;
-            usedArray = []
-            attempts = 10;
-      }
+        }
 
-      //HANDELING INCORRECT GUESSES - OUTPUT
-      //this 'if' prevents a letter selected a 2nd time from being written to the usedArray again, although it still counts as a guess
-      if (usedArray.indexOf(playerGuess) >= 0) {
+        else {
+            guessesSofar.push(playerGuess);
+            document.getElementById('playerGuess').innerHTML = guessesSofar;
+            console.log(guessesSofar);
 
-      } else {
-            //this pushes the players incorrect guess to the usedArray and writes it to the HTML
-            usedArray.push(playerGuess);
-            document.getElementById('playerGuess').innerHTML = usedArray;
-            console.log(usedArray);
+        }
 
-      }
-      //OUTPUT TO HTML
-      //these statements write the values/scores generated to the HTML
-      document.getElementById('won').innerHTML = won;
-      document.getElementById('lost').innerHTML = lost;
-      document.getElementById('attempts').innerHTML = attempts;
+        document.getElementById('win').innerHTML = win;
+        document.getElementById('loss').innerHTML = loss;
+        document.getElementById('guesses').innerHTML = guesses;
+    }
 
-}
+    
